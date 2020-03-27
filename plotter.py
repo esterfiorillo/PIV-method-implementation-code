b21183e2fda0fb956c78e1e1ceb4a9b2669a35c1
Ester Fiorillo <32225398+esterfiorillo@users.noreply.github.com>
2020-03-27 15:39:09 -0300

    Add files via upload

---
+++ plotter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 11:37:19 2020
+
+@author: esterfiorillo
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+entrada1 = pd.read_csv('dpx_resultado1', sep = '\t')
+a = entrada1.as_matrix()
+a = np.array(a)
+
+entrada2 = pd.read_csv('dpy_resultado1', sep = '\t')
+b = entrada2.as_matrix()
+b = np.array(b)
+
+plt.quiver(a, b)
