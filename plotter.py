e2e23b3581982198b90008eae5eb4aed40fa03eb
Ester Fiorillo Rocha de Resende <esterfiorillo@localhost.localdomain>
2020-04-21 17:43:16 -0300

    Uptade plotter to show reescaled vector map

--- plotter.py
+++ plotter.py
@@ -10,6 +10,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+ws = input("Type the final interrogation window size:")
+ws = int (ws)
+
 entrada1 = pd.read_csv('dpx_resultado_multigrid', sep = '\t')
 a = entrada1.as_matrix()
 a = np.array(a)
@@ -18,4 +21,18 @@ entrada2 = pd.read_csv('dpy_resultado_multigrid', sep = '\t')
 b = entrada2.as_matrix()
 b = np.array(b)
 
-plt.quiver(a, b)
+tam_y, tam_x= np.shape(a)
+x = np.arange(0, tam_x, 1)
+y = np.arange(0, tam_y, 1)
+x, y= np.meshgrid(x, y)
+
+def reescaling(x, y, a, b, ws):
+    x = x*ws
+    y= y*ws
+    a = a*ws
+    b = b*ws
+    return a, b, x, y
+
+a, b, x, y = reescaling(x, y, a, b, ws)
+
+plt.quiver(x, y, a, b)
